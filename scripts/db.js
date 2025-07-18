3c6feeb9404945524f96600df868aa0c782946b0
fReEsPiRiT-Spiritsoft <p.schmidt.391994@gmail.com>
2025-07-18 15:16:57 +0200

    added pictures for foods

---
+++ scripts/db.js
@@ -0,0 +1,28 @@
+let dishes = [
+    {
+        "name": "Pizza Margherita",
+        "price": 8.50,
+        "description": "Klassische Pizza mit Tomatensauce, Mozzarella und frischem Basilikum.",
+        "image": "img/pizza-margherita.jpg", 
+    },
+    {
+        "name": "Spaghetti Carbonara",
+        "price": 10.00,
+        "description": "Spaghetti mit einer cremigen Sauce aus Eiern, Käse, Speck und schwarzem Pfeffer.",
+        "image": "img/spaghetti-carbonara.jpg"
+    },
+    {
+        "name": "Caesar Salad",
+        "price": 7.00,
+        "description": "Frischer Römersalat mit Croutons, Parmesan und Caesar-Dressing.",
+        "image": "img/caesar-salad.jpg"
+    },
+    {
+        "name": "Tiramisu",
+        "price": 5.50,
+        "description": "Klassisches italienisches Dessert mit Mascarpone, Kaffee und Kakaopulver.",
+        "image": "img/tiramisu.jpg"
+    }
+];
+
+let orders = []
